69e901376b062b0621080f2c95aa8db49b583d4d
seongh06 <seongh06@naver.com>
2026-04-01 15:09:33 +0900

    [FEAT/#42] Alerts 구현 (추후 개발 필요)

--- feature/home/src/commonMain/kotlin/every/lol/com/feature/home/model/HomeUiModel.kt
+++ feature/home/src/commonMain/kotlin/every/lol/com/feature/home/model/HomeUiModel.kt
@@ -1,6 +1,7 @@
 package every.lol.com.feature.home.model
 
 import androidx.compose.runtime.Immutable
+import every.lol.com.core.model.HomeAlerts
 import every.lol.com.core.model.HomeNews
 import every.lol.com.core.model.HomeRanking
 import every.lol.com.core.model.HomeTodayMatch
@@ -14,6 +15,8 @@ sealed interface HomeUiState {
         val matches: HomeTodayMatch ?= null,
         val ranking: HomeRanking?= null,
         val news: HomeNews?=null,
+        val alerts: HomeAlerts?=null,
+        val alertsMessage: String?=null,
         val isRefreshing: Boolean = false
     ) : HomeUiState
 
